18c9f28abf401befd13a7744bdb635934bc1052f
Jacob Rothstein <hi@jbr.me>
2021-03-05 21:54:14 -0800

    fix smol-server example

--- smol-server/examples/smol-server.rs
+++ smol-server/examples/smol-server.rs
@@ -1,10 +1,4 @@
-use myco::Conn;
-use std::time::Duration;
-
 pub fn main() {
     env_logger::init();
-    myco_smol_server::run(|conn: Conn| async move {
-        smol::Timer::after(Duration::from_secs(1)).await;
-        conn.ok("hello")
-    });
+    myco_smol_server::run(|conn: myco::Conn| async move { conn.ok("hello") });
 }
