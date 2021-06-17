763ab2e6cbd38d5082d958c81855bd281d1ab3b9
Jacob Rothstein <hi@jbr.me>
2021-06-17 13:26:50 -0700

    logger improvements

--- logger/src/lib.rs
+++ logger/src/lib.rs
@@ -1,50 +1,121 @@
-#![forbid(unsafe_code)]
-#![deny(
-    missing_copy_implementations,
-    missing_crate_level_docs,
-    missing_debug_implementations,
-    missing_docs,
-    nonstandard_style,
-    unused_qualifications
-)]
+// #![forbid(unsafe_code)]
+// #![warn(
+//     missing_crate_level_docs,
+//     missing_debug_implementations,
+//     nonstandard_style,
+//     unused_qualifications
+// )]
 
-/*!
-Development-mode logger for trillium
+pub use crate::formatters::{apache_combined, apache_common, dev_formatter};
+use std::fmt::Display;
+use trillium::{async_trait, Conn, Handler, Info};
+pub mod formatters;
 
-stability note: this is basically just a proof of concept, and the
-interface will likely change quite a bit before stabilizing
-*/
+#[derive(Clone, Copy, Debug)]
+pub enum ColorMode {
+    Auto,
+    On,
+    Off,
+}
 
-use colored::*;
-use size::{Base, Size, Style};
-use std::time::Instant;
-use trillium::{async_trait, http_types::StatusCode, Conn, Handler, Info};
-
-/**
-Development-mode logger for trillium
-
-stability note: this is basically just a proof of concept, and the
-interface will likely change quite a bit before stabilizing
-*/
-#[derive(Clone, Copy, Debug, Default)]
-pub struct Logger(());
-impl Logger {
-    /// construct a new logger
-    pub fn new() -> Self {
-        Self(())
+impl ColorMode {
+    pub fn is_enabled(&self) -> bool {
+        match self {
+            ColorMode::Auto => atty::is(atty::Stream::Stdout),
+            ColorMode::On => true,
+            ColorMode::Off => false,
+        }
     }
 }
 
-struct LoggerRan;
+impl Default for ColorMode {
+    fn default() -> Self {
+        Self::Auto
+    }
+}
 
-#[async_trait]
-impl Handler for Logger {
-    async fn run(&self, conn: Conn) -> Conn {
-        conn.with_state(LoggerRan)
+#[derive(Clone, Copy, Debug)]
+#[non_exhaustive]
+pub enum Target {
+    Logger(log::Level),
+    Stdout,
+}
+
+impl Target {
+    pub fn write(&self, data: impl Display) {
+        match self {
+            Target::Logger(level) => {
+                log::log!(*level, "{}", data);
+            }
+
+            Target::Stdout => {
+                println!("{}", data);
+            }
+        }
+    }
+}
+
+impl Default for Target {
+    fn default() -> Self {
+        Self::Logger(log::Level::Info)
+    }
+}
+
+pub trait LogFormatter: Send + Sync {
+    type Output: Display + Send + Sync + 'static;
+    fn format(&self, conn: &Conn, color: bool) -> Self::Output;
+}
+
+pub struct Logger<F> {
+    format: F,
+    color_mode: ColorMode,
+    target: Target,
+}
+
+impl Logger<()> {
+    pub fn new() -> Logger<impl LogFormatter> {
+        Logger {
+            format: dev_formatter,
+            color_mode: ColorMode::Auto,
+            target: Target::Logger(log::Level::Info),
+        }
+    }
+}
+
+impl<T> Logger<T> {
+    pub fn with_formatter<Formatter: LogFormatter>(
+        self,
+        formatter: Formatter,
+    ) -> Logger<Formatter> {
+        Logger {
+            format: formatter,
+            color_mode: self.color_mode,
+            target: self.target,
+        }
+    }
+}
+
+impl<F: LogFormatter> Logger<F> {
+    pub fn with_color_mode(mut self, color_mode: ColorMode) -> Self {
+        self.color_mode = color_mode;
+        self
     }
 
+    pub fn with_target(mut self, target: Target) -> Self {
+        self.target = target;
+        self
+    }
+}
+
+struct LoggerWasRun;
+
+#[async_trait]
+impl<F> Handler for Logger<F>
+where
+    F: LogFormatter + 'static,
+{
     async fn init(&mut self, info: &mut Info) {
-        log::info!(
+        self.target.write(&format!(
             "
 🌱🦀🌱 {} started
 Listening at {}{}
@@ -54,43 +125,20 @@ Control-C to quit",
             info.listener_description(),
             info.tcp_socket_addr()
                 .map(|s| format!(" (bound as tcp://{})", s))
-                .unwrap_or_default()
-        );
+                .unwrap_or_default(),
+        ));
+    }
+    async fn run(&self, conn: Conn) -> Conn {
+        conn.with_state(LoggerWasRun)
     }
 
     async fn before_send(&self, mut conn: Conn) -> Conn {
-        if conn.take_state::<LoggerRan>().is_some() {
-            let start_time = conn.inner().start_time();
-            let method = conn.method();
-            let status = conn.status().unwrap_or(StatusCode::NotFound);
-            let len = conn
-                .response_len()
-                .map(|l| {
-                    Size::to_string(&Size::Bytes(l), Base::Base10, Style::Smart).replace(" ", "")
-                })
-                .unwrap_or_else(|| String::from("-"));
-
-            let url = String::from(conn.path());
-
-            let status_string = (status as u16).to_string().color(match status as u16 {
-                200..=299 => "green",
-                300..=399 => "cyan",
-                400..=499 => "yellow",
-                500..=599 => "red",
-                _ => "white",
-            });
-
-            conn.inner_mut().after_send(move |_| {
-                log::info!(
-                    r#"{method} {url} {status} {response_time:?} {len}"#,
-                    response_time = Instant::now() - start_time,
-                    method = method,
-                    url = url,
-                    status = status_string,
-                    len = len,
-                );
-            });
+        if conn.state::<LoggerWasRun>().is_some() {
+            let target = self.target;
+            let output = self.format.format(&conn, self.color_mode.is_enabled());
+            conn.inner_mut().after_send(move |_| target.write(output));
         }
+
         conn
     }
 }
