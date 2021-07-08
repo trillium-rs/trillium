0c8495cd2c1d77cb09bff15b04d2d2d09c1ba14a
Jacob Rothstein <hi@jbr.me>
2021-07-07 21:10:50 -0700

    trillium-method-override@0.1.0

---
+++ method-override/src/lib.rs
@@ -0,0 +1,94 @@
+/*!
+# Trillium method override handler
+
+*/
+#![forbid(unsafe_code)]
+#![deny(
+    missing_copy_implementations,
+    rustdoc::missing_crate_level_docs,
+    missing_debug_implementations,
+    missing_docs,
+    nonstandard_style,
+    unused_qualifications
+)]
+
+use querystrong::QueryStrong;
+use std::{collections::HashSet, convert::TryInto, fmt::Debug, iter::FromIterator};
+use trillium::{async_trait, conn_unwrap, http_types::Method, Conn, Handler};
+
+/**
+Trillium method override handler
+
+See crate-level docs for an explanation
+*/
+#[derive(Clone, Debug)]
+pub struct MethodOverride {
+    param: &'static str,
+    allowed_methods: HashSet<Method>,
+}
+
+impl Default for MethodOverride {
+    fn default() -> Self {
+        Self {
+            param: "_method",
+            allowed_methods: HashSet::from_iter([Method::Put, Method::Patch, Method::Delete]),
+        }
+    }
+}
+
+impl MethodOverride {
+    /// constructs a new MethodOverride handler with default allowed methods and param name
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /**
+    replace the default allowed methods with the provided list of methods
+
+    default: `put`, `patch`, `delete`
+
+    ```
+    # use trillium_method_override::MethodOverride;
+    let handler = MethodOverride::new().with_allowed_methods(["put", "patch"]);
+    ```
+    */
+    pub fn with_allowed_methods<M>(mut self, methods: impl IntoIterator<Item = M>) -> Self
+    where
+        M: TryInto<Method>,
+        <M as TryInto<Method>>::Error: Debug,
+    {
+        self.allowed_methods = methods.into_iter().map(|m| m.try_into().unwrap()).collect();
+        self
+    }
+
+    /**
+    replace the default param name with the provided param name
+
+    default: `_method`
+    ```
+    # use trillium_method_override::MethodOverride;
+    let handler = MethodOverride::new().with_param_name("_http_method");
+    ```
+    */
+
+    pub fn with_param_name(mut self, param_name: &'static str) -> Self {
+        self.param = param_name;
+        self
+    }
+}
+
+#[async_trait]
+impl Handler for MethodOverride {
+    async fn run(&self, mut conn: Conn) -> Conn {
+        if conn.method() != Method::Post {
+            return conn;
+        }
+        let qs = conn_unwrap!(conn, QueryStrong::parse(conn.querystring()).ok());
+        let method_str = conn_unwrap!(conn, qs.get_str(self.param));
+        let method: Method = conn_unwrap!(conn, method_str.try_into().ok());
+        if self.allowed_methods.contains(&method) {
+            conn.inner_mut().set_method(method);
+        }
+        conn
+    }
+}
---
+++ method-override/tests/tests.rs
@@ -0,0 +1,49 @@
+use trillium::{http_types::Method, Conn};
+use trillium_method_override::*;
+use trillium_testing::prelude::*;
+
+async fn test_handler(conn: Conn) -> Conn {
+    match (conn.method(), conn.path()) {
+        (Method::Delete, _) => conn.ok("you did a delete"),
+        (Method::Post, _) => conn.ok("it was a post"),
+        (Method::Patch, _) => conn.ok("adams"),
+        (Method::Put, _) => conn.ok("put and call"),
+        _ => conn,
+    }
+}
+
+#[test]
+fn test() {
+    let app = (MethodOverride::new(), test_handler);
+
+    assert_ok!(post("/?_method=delete").on(&app), "you did a delete");
+    assert_ok!(
+        post("/?a=b&_method=delete&c=d").on(&app),
+        "you did a delete"
+    );
+
+    assert_ok!(post("/?_method=connect").on(&app), "it was a post");
+    assert_ok!(post("/?_method!!-=/=connect").on(&app), "it was a post");
+
+    assert_not_handled!(get("/?_method=delete").on(&app));
+}
+
+#[test]
+fn with_limited_allowed_methods() {
+    let app = (
+        MethodOverride::new().with_allowed_methods(["put", "patch"]),
+        test_handler,
+    );
+
+    assert_ok!(post("/?_method=put").on(&app), "put and call");
+    assert_ok!(post("/?a=b&_method=patch&c=d").on(&app), "adams");
+
+    assert_ok!(post("/?_method=delete").on(&app), "it was a post");
+}
+
+#[test]
+fn with_a_different_param_name() {
+    let app = (MethodOverride::new().with_param_name("verb"), test_handler);
+    assert_ok!(post("/?verb=delete").on(&app), "you did a delete");
+    assert_ok!(post("/?_method=delete").on(&app), "it was a post");
+}
