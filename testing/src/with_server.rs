8e6723b56dc9fa2f1e5e0b597cd39358db86f105
Jacob Rothstein <hi@jbr.me>
2021-09-10 17:11:26 -0700

    testing: use init and a channel to wait for the server to come up

--- testing/src/with_server.rs
+++ testing/src/with_server.rs
@@ -1,142 +1,21 @@
-use crate::{block_on, Url};
-use std::{future::Future, time::Duration};
-use trillium::Handler;
-use trillium_server_common::Stopper;
-
 cfg_if::cfg_if! {
     if #[cfg(feature = "smol")] {
-        use trillium_smol::{async_io::Timer, async_global_executor::spawn};
-
-        /**
-        Starts a trillium handler bound to a random available port on
-        localhost, run the async tests provided as the second
-        argument, and then shut down the server. useful for full
-        integration tests that actually exercise the tcp layer.
-
-        See
-        [`trillium_client::Conn`](https://docs.trillium.rs/trillium_client/struct.conn)
-        for usage examples.
-
-        stability note: this doesn't really feel like it fits in the testing
-        crate, as it would not work well with a tokio-specific handler. it may
-        go away entirely at some point, or be moved to the trillium_smol crate
-         */
-        pub fn with_server<H, Fun, Fut>(handler: H, tests: Fun)
-        where
-            H: Handler,
-            Fun: Fn(Url) -> Fut,
-            Fut: Future<Output = Result<(), Box<dyn std::error::Error>>>,
-        {
-            block_on(async move {
-                let port = portpicker::pick_unused_port().expect("could not pick a port");
-                let url = format!("http://localhost:{}", port).parse().unwrap();
-                let stopper = Stopper::new();
-
-                let server_future = spawn(
-                    trillium_smol::config()
-                        .with_host("localhost")
-                        .with_port(port)
-                        .with_stopper(stopper.clone())
-                        .run_async(handler),
-                );
-
-                Timer::after(Duration::from_millis(500)).await;
-                let result = tests(url).await;
-                stopper.stop();
-                server_future.await;
-                result.unwrap()
-            })
-        }
+        mod smol;
+        pub use smol::with_server;
     } else if #[cfg(feature = "async-std")] {
-        use trillium_async_std::async_std::task::{sleep, spawn};
-
-        /**
-        Starts a trillium handler bound to a random available port on
-        localhost, run the async tests provided as the second
-        argument, and then shut down the server. useful for full
-        integration tests that actually exercise the tcp layer.
-
-        See
-        [`trillium_client::Conn`](https://docs.trillium.rs/trillium_client/struct.conn)
-        for usage examples.
-         */
-        pub fn with_server<H, Fun, Fut>(handler: H, tests: Fun)
-        where
-            H: Handler,
-            Fun: Fn(Url) -> Fut,
-            Fut: Future<Output = Result<(), Box<dyn std::error::Error>>>,
-        {
-            block_on(async move {
-                let port = portpicker::pick_unused_port().expect("could not pick a port");
-                let url = format!("http://localhost:{}", port).parse().unwrap();
-                let stopper = Stopper::new();
-
-                let server_future = spawn(
-                    trillium_async_std::config()
-                        .with_host("localhost")
-                        .with_port(port)
-                        .with_stopper(stopper.clone())
-                        .run_async(handler),
-                );
-
-                sleep(Duration::from_millis(500)).await;
-                let result = tests(url).await;
-                stopper.stop();
-                server_future.await;
-                result.unwrap()
-            })
-        }
-
+        mod async_std;
+        pub use async_std::with_server;
     } else if #[cfg(feature = "tokio")] {
-        use trillium_tokio::tokio::{time::sleep, task::spawn};
-
-        /**
-        Starts a trillium handler bound to a random available port on
-        localhost, run the async tests provided as the second
-        argument, and then shut down the server. useful for full
-        integration tests that actually exercise the tcp layer.
-
-        See
-        [`trillium_client::Conn`](https://docs.trillium.rs/trillium_client/struct.conn)
-        for usage examples.
-         */
-        pub fn with_server<H, Fun, Fut>(handler: H, tests: Fun)
-        where
-            H: Handler,
-            Fun: Fn(Url) -> Fut,
-            Fut: Future<Output = Result<(), Box<dyn std::error::Error>>>,
-        {
-            block_on(async move {
-                let port = portpicker::pick_unused_port().expect("could not pick a port");
-                let url = format!("http://localhost:{}", port).parse().unwrap();
-                let stopper = Stopper::new();
-
-                let server_future = spawn(
-                    trillium_tokio::config()
-                        .with_host("localhost")
-                        .with_port(port)
-                        .with_stopper(stopper.clone())
-                        .run_async(handler),
-                );
-
-                sleep(Duration::from_millis(500)).await;
-                let result = tests(url).await;
-                stopper.stop();
-                drop(server_future.await);
-                result.unwrap()
-            })
-        }
-    } else {
+        mod tokio;
+        pub use tokio::with_server;
+   } else {
         pub fn with_server<H, Fun, Fut>(_handler: H, _tests: Fun)
         where
-            H: Handler,
-            Fun: Fn(Url) -> Fut,
-            Fut: Future<Output = Result<(), Box<dyn std::error::Error>>>,
+            H: trillium::Handler,
+            Fun: Fn(crate::Url) -> Fut,
+            Fut: std::future::Future<Output = Result<(), Box<dyn std::error::Error>>>,
         {
-            block_on(async {});
-            let duration = Duration::ZERO;
-            let stopper = Stopper::new();
+            panic!()
         }
-        compile_error!("must enable smol, async-std, or tokio feature");
     }
 }
---
+++ testing/src/with_server/async_std.rs
@@ -0,0 +1,48 @@
+use crate::{block_on, Url};
+use std::future::Future;
+use trillium::Handler;
+use trillium_async_std::async_std::task::spawn;
+use trillium_server_common::Stopper;
+
+/**
+Starts a trillium handler bound to a random available port on
+localhost, run the async tests provided as the second
+argument, and then shut down the server. useful for full
+integration tests that actually exercise the tcp layer.
+
+See
+[`trillium_client::Conn`](https://docs.trillium.rs/trillium_client/struct.conn)
+for usage examples.
+ */
+pub fn with_server<H, Fun, Fut>(handler: H, tests: Fun)
+where
+    H: Handler,
+    Fun: Fn(Url) -> Fut,
+    Fut: Future<Output = Result<(), Box<dyn std::error::Error>>>,
+{
+    block_on(async move {
+        let port = portpicker::pick_unused_port().expect("could not pick a port");
+        let url = format!("http://localhost:{}", port).parse().unwrap();
+        let stopper = Stopper::new();
+        let (s, r) = async_channel::bounded(1);
+        let init = trillium::Init::new(move |_| {
+            let s = s.clone();
+            async move {
+                s.send(()).await.unwrap();
+            }
+        });
+
+        let server_future = spawn(
+            trillium_async_std::config()
+                .with_host("localhost")
+                .with_port(port)
+                .with_stopper(stopper.clone())
+                .run_async((init, handler)),
+        );
+        r.recv().await.unwrap();
+        let result = tests(url).await;
+        stopper.stop();
+        server_future.await;
+        result.unwrap()
+    })
+}
