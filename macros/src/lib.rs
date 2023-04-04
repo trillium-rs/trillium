18654e115a0ec6da53d6ed06c97539ad64b8e733
Jacob Rothstein <hi@jbr.me>
2023-04-03 18:38:02 -0700

    add skipping to trillium macros

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -9,89 +9,22 @@
 )]
 #![warn(missing_docs, clippy::nursery, clippy::cargo)]
 #![allow(clippy::must_use_candidate, clippy::module_name_repetitions)]
-
-/*!
-# Welcome to the `trillium-macros` crate!
-
-This crate currently offers a derive macro for Handler that can be
-used to delegate Handler behavior to a contained Handler
-type. Currently it only works for structs, but will eventually support
-enums as well. Note that it will only delegate to a single inner Handler type.
-
-In the case of a newtype struct or named struct with only a single
-field, `#[derive(Handler)]` is all that's required. If there is more
-than one field in the struct, annotate exactly one of them with
-#[handler].
-
-As of v0.0.2, deriving Handler makes an effort at adding Handler
-bounds to generics contained within the `#[handler]` type. It may be
-overzealous in adding those bounds, in which case you'll need to
-implement Handler yourself.
-
-
-```rust
-
-// for these examples, we are using a `&'static str` as the handler type.
-
-use trillium_macros::Handler;
-# fn assert_handler(_h: impl trillium::Handler) {}
-
-#[derive(Handler)]
-struct NewType(&'static str);
-assert_handler(NewType("yep"));
-
-#[derive(Handler)]
-struct TwoTypes(usize, #[handler] &'static str);
-assert_handler(TwoTypes(2, "yep"));
-
-#[derive(Handler)]
-struct NamedSingleField {
-    this_is_the_handler: &'static str,
-}
-assert_handler(NamedSingleField { this_is_the_handler: "yep" });
-
-
-#[derive(Handler)]
-struct NamedMultiField {
-    not_handler: usize,
-    #[handler]
-    inner_handler: &'static str,
-    also_not_handler: usize,
-}
-
-assert_handler(NamedMultiField {
-    not_handler: 1,
-    inner_handler: "yep",
-    also_not_handler: 3,
-});
-
-#[derive(Handler)]
-struct Generic<G>(G);
-assert_handler(Generic("hi"));
-assert_handler(Generic(trillium::Status::Ok));
-
-
-#[derive(Handler)]
-struct ContainsHandler<A, B> {
-    the_handler: (A, B)
-}
-assert_handler(ContainsHandler {
-    the_handler: ("hi", trillium::Status::Ok)
-});
-
-```
-*/
-use std::collections::HashSet;
+#![doc = include_str!("../README.md")]
 
 use proc_macro::TokenStream;
 use quote::quote;
+use std::collections::HashSet;
 use syn::{
+    parse::Parse,
     parse_macro_input, parse_quote,
     punctuated::Punctuated,
+    spanned::Spanned,
     token::{Comma, Where},
     visit::{visit_type_path, Visit},
-    Data, DeriveInput, Field, Ident, Index, Type, TypePath, WhereClause,
+    Attribute, Data, DeriveInput, Error, Expr, ExprArray, ExprAssign, ExprPath, Field, Ident,
+    Index, Member, Meta, Path, Type, TypePath, WhereClause,
 };
+
 fn is_required_generic_for_type(ty: &Type, generic: &Ident) -> bool {
     struct PathVisitor<'g> {
         generic: &'g Ident,
@@ -120,68 +53,159 @@ fn is_required_generic_for_type(ty: &Type, generic: &Ident) -> bool {
     path_visitor.generic_is_required
 }
 
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+enum Skippable {
+    Run,
+    Init,
+    BeforeSend,
+    HasUpgrade,
+    Upgrade,
+    Name,
+}
+
+impl TryFrom<&Path> for Skippable {
+    type Error = Error;
+    fn try_from(path: &Path) -> Result<Self, Self::Error> {
+        if path.is_ident("run") {
+            Ok(Self::Run)
+        } else if path.is_ident("init") {
+            Ok(Self::Init)
+        } else if path.is_ident("before_send") {
+            Ok(Self::BeforeSend)
+        } else if path.is_ident("has_upgrade") {
+            Ok(Self::HasUpgrade)
+        } else if path.is_ident("upgrade") {
+            Ok(Self::Upgrade)
+        } else if path.is_ident("name") {
+            Ok(Self::Name)
+        } else {
+            Err(Error::new(path.span(), "unrecognized skip"))
+        }
+    }
+}
+
+struct DeriveOptions {
+    skips: Vec<Skippable>,
+    input: DeriveInput,
+    field: Field,
+    field_index: usize,
+}
+
+fn skips<'a, I: Iterator<Item = &'a Expr>>(iter: I) -> syn::Result<Vec<Skippable>> {
+    iter.map(|expr| match expr {
+        Expr::Path(ExprPath { path, .. }) => path.try_into(),
+        _ => Err(Error::new(expr.span(), "unrecognized skip. valid options are run, init, before_send, name, has_upgrade, and upgrade")),
+    })
+    .collect()
+}
+
+fn parse_attribute(attr: &Attribute) -> syn::Result<Option<Vec<Skippable>>> {
+    if attr.path().is_ident("handler") {
+        match &attr.meta {
+            Meta::Path(_) => Ok(Some(vec![])),
+            Meta::List(metalist) => {
+                let tokens = metalist.tokens.clone();
+                let ExprAssign { left, right, .. } = syn::parse(tokens.into())?;
+                match (*left, *right) {
+                    (Expr::Path(ExprPath { path: left, .. }), right @ Expr::Path(_))
+                        if left.is_ident("skip") =>
+                    {
+                        Ok(Some(skips(std::iter::once(&right))?))
+                    }
+
+                    (
+                        Expr::Path(ExprPath { path: left, .. }),
+                        Expr::Array(ExprArray { elems: right, .. }),
+                    ) if left.is_ident("skip") => Ok(Some(skips(right.iter())?)),
+
+                    (_x, _y) => Err(Error::new(
+                        metalist.span(),
+                        format!("unrecognized #[handler] attributes"),
+                    )),
+                }
+            }
+            Meta::NameValue(nv) => Err(Error::new(
+                nv.span(),
+                format!("unrecognized #[handler] attributes"),
+            )),
+        }
+    } else {
+        Ok(None)
+    }
+}
+
+fn generics(field: &Field, input: &DeriveInput) -> Vec<Ident> {
+    input
+        .generics
+        .type_params()
+        .filter_map(|g| {
+            if is_required_generic_for_type(&field.ty, &g.ident) {
+                Some(g.ident.clone())
+            } else {
+                None
+            }
+        })
+        .collect::<HashSet<_>>()
+        .into_iter()
+        .collect()
+}
+
+impl Parse for DeriveOptions {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let input = DeriveInput::parse(input)?;
+        let Data::Struct(ds) = &input.data else { return Err(Error::new(input.span(), "second erro")) };
+
+        for (field_index, field) in ds.fields.iter().enumerate() {
+            for attr in &field.attrs {
+                if let Some(skips) = parse_attribute(&attr)? {
+                    let field = field.clone();
+                    return Ok(DeriveOptions {
+                        skips,
+                        input,
+                        field,
+                        field_index,
+                    });
+                }
+            }
+        }
+
+        if ds.fields.len() == 1 {
+            let field = ds
+                .fields
+                .iter()
+                .next()
+                .expect("len == 1 should have one element")
+                .clone();
+            Ok(DeriveOptions {
+                skips: vec![],
+                input,
+                field,
+                field_index: 0,
+            })
+        } else {
+            Err(Error::new(
+                input.span(),
+                "Structs with more than one field need a #[handler] annotation",
+            ))
+        }
+    }
+}
+
 /// see crate docs
 #[proc_macro_derive(Handler, attributes(handler))]
 pub fn derive_handler(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as DeriveInput);
+    let DeriveOptions {
+        skips,
+        field,
+        input,
+        field_index,
+    } = parse_macro_input!(input as DeriveOptions);
 
     let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
 
-    let name = input.ident;
+    let generics = generics(&field, &input);
 
-    let mut generics = HashSet::new();
-
-    let mut save_generics = |field: &Field| {
-        generics = input
-            .generics
-            .type_params()
-            .filter_map(|g| {
-                if is_required_generic_for_type(&field.ty, &g.ident) {
-                    Some(g.ident.clone())
-                } else {
-                    None
-                }
-            })
-            .collect::<HashSet<_>>();
-    };
-
-    let handler = match input.data {
-        Data::Struct(ds) => {
-            if ds.fields.len() == 1 {
-                let field = ds
-                    .fields
-                    .into_iter()
-                    .next()
-                    .expect("len == 1 should have only one element");
-
-                save_generics(&field);
-
-                field
-                    .ident
-                    .map_or_else(|| quote!(self.0), |field| quote!(self.#field))
-            } else {
-                ds
-                    .fields
-                    .into_iter()
-                    .enumerate()
-                    .find_map(|(n, f)| {
-                        if f.attrs.iter().any(|attr| attr.path().is_ident("handler")) {
-                            save_generics(&f);
-                            Some(f.ident.map_or_else(|| {
-                                let n = Index::from(n);
-                                quote!(self.#n)
-                            }, |ident| quote!(self.#ident)))
-                        } else {
-                            None
-                        }
-                    })
-                    .expect("for structs with more than one field, please annotate one of them with #[handler]")
-            }
-        }
-        _ => panic!("Only structs are currently supported by derive(Handler). Enums coming soon!"),
-    };
-
-    let name_string = name.to_string();
+    let struct_name = input.ident;
 
     let mut where_clause = where_clause.map_or_else(
         || WhereClause {
@@ -202,32 +226,58 @@ pub fn derive_handler(input: TokenStream) -> TokenStream {
         .predicates
         .push_value(parse_quote! { Self: Send + Sync + 'static });
 
+    let handler = field
+        .ident
+        .map_or_else(|| Member::Unnamed(Index::from(field_index)), Member::Named);
+
+    let handler = quote!(self.#handler);
+
+    let run = if skips.contains(&Skippable::Run) {
+        quote!(Self::run(&self, conn))
+    } else {
+        quote!(trillium::Handler::run(&#handler, conn))
+    };
+
+    let init = if skips.contains(&Skippable::Init) {
+        quote!(Self::init(&mut self, info))
+    } else {
+        quote!(trillium::Handler::init(&mut #handler, info))
+    };
+
+    let before_send = if skips.contains(&Skippable::BeforeSend) {
+        quote!(Self::before_send(&self, conn))
+    } else {
+        quote!(trillium::Handler::before_send(&#handler, conn))
+    };
+
+    let name = if skips.contains(&Skippable::Name) {
+        quote!(Self::name(&self))
+    } else {
+        let name_string = struct_name.to_string();
+        quote!(format!("{} ({})", #name_string, trillium::Handler::name(&#handler)).into())
+    };
+
+    let has_upgrade = if skips.contains(&Skippable::HasUpgrade) {
+        quote!(Self::has_upgrade(&self, upgrade))
+    } else {
+        quote!(trillium::Handler::has_upgrade(&#handler, upgrade))
+    };
+
+    let upgrade = if skips.contains(&Skippable::Upgrade) {
+        quote!(Self::upgrade(&self, upgrade))
+    } else {
+        quote!(trillium::Handler::upgrade(&#handler, upgrade))
+    };
+
     quote! {
         #[trillium::async_trait]
-        impl #impl_generics trillium::Handler for #name #ty_generics #where_clause {
-            async fn run(&self, conn: trillium::Conn) -> trillium::Conn {
-                trillium::Handler::run(&#handler, conn).await
-            }
-
-            async fn init(&mut self, info: &mut trillium::Info) {
-                trillium::Handler::init(&mut #handler, info).await;
-            }
-
-            async fn before_send(&self, conn: trillium::Conn) -> trillium::Conn {
-                trillium::Handler::before_send(&#handler, conn).await
-            }
-
-            fn name(&self) -> std::borrow::Cow<'static, str> {
-                format!("{} ({})", #name_string, trillium::Handler::name(&#handler)).into()
-            }
-
-            fn has_upgrade(&self, upgrade: &trillium::Upgrade) -> bool {
-                trillium::Handler::has_upgrade(&#handler, upgrade)
-            }
-
-            async fn upgrade(&self, upgrade: trillium::Upgrade) {
-                trillium::Handler::upgrade(&#handler, upgrade).await;
-            }
+        impl #impl_generics trillium::Handler for #struct_name #ty_generics #where_clause {
+            async fn run(&self, conn: trillium::Conn) -> trillium::Conn { #run.await }
+            async fn init(&mut self, info: &mut trillium::Info) { #init.await; }
+            async fn before_send(&self, conn: trillium::Conn) -> trillium::Conn { #before_send.await }
+            fn name(&self) -> std::borrow::Cow<'static, str> { #name }
+            fn has_upgrade(&self, upgrade: &trillium::Upgrade) -> bool { #has_upgrade }
+            async fn upgrade(&self, upgrade: trillium::Upgrade) { #upgrade.await }
         }
     }
     .into()
