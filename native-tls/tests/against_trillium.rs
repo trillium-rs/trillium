0a70cea69f770377181f6d9c00762ad46892f564
Jacob Rothstein <hi@jbr.me>
2023-04-24 16:33:29 -0700

    run mkcert in ubuntu only, use it optionally in tests

--- native-tls/tests/against_trillium.rs
+++ native-tls/tests/against_trillium.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, future::Future};
+use std::{env::var, error::Error, fs::read, future::Future};
 use test_harness::test;
 use trillium_client::Client;
 use trillium_native_tls::{NativeTlsAcceptor, NativeTlsConfig};
@@ -10,6 +10,14 @@ fn handler() -> impl trillium::Handler {
     "ok"
 }
 
+fn pem_and_key() -> Option<(Vec<u8>, Vec<u8>)> {
+    let root = var("CARGO_MANIFEST_DIR").ok()?;
+    Some((
+        read(format!("{root}/../localhost.pem")).ok()?,
+        read(format!("{root}/../localhost-key.pem")).ok()?,
+    ))
+}
+
 pub fn with_native_tls_server<Fun, Fut>(tests: Fun)
 where
     Fun: FnOnce(Url) -> Fut,
@@ -18,13 +26,14 @@ where
     block_on(async move {
         let port = portpicker::pick_unused_port().expect("could not pick a port");
         let url = format!("https://localhost:{port}").parse().unwrap();
+        let Some((pem, key)) = pem_and_key() else {
+            return
+        };
+
         let handle = config()
             .with_host("localhost")
             .with_port(port)
-            .with_acceptor(NativeTlsAcceptor::from_pkcs12(
-                include_bytes!("./identity.p12"),
-                "changeit",
-            ))
+            .with_acceptor(NativeTlsAcceptor::from_pkcs8(&pem, &key))
             .spawn(handler());
         handle.info().await;
         tests(url).await.unwrap();
@@ -40,13 +49,14 @@ where
     block_on(async move {
         let port = portpicker::pick_unused_port().expect("could not pick a port");
         let url = format!("https://localhost:{port}").parse().unwrap();
+        let Some((pem, key)) = pem_and_key() else {
+            return
+        };
+
         let handle = config()
             .with_host("localhost")
             .with_port(port)
-            .with_acceptor(RustlsAcceptor::from_single_cert(
-                include_bytes!("./cert.pem"),
-                include_bytes!("./key.pem"),
-            ))
+            .with_acceptor(RustlsAcceptor::from_single_cert(&pem, &key))
             .spawn(handler());
         handle.info().await;
         tests(url).await.unwrap();
