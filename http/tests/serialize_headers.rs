948d7410de618f2f5d61dbb4f0571f544a2fce2d
Jacob Rothstein <hi@jbr.me>
2023-12-20 14:19:23 -0800

    add test for serialization

---
+++ http/tests/serialize_headers.rs
@@ -0,0 +1,26 @@
+#![cfg(feature = "serde")]
+
+#[test]
+fn header_serialization() {
+    let mut headers = trillium_http::Headers::new();
+    headers.insert(trillium_http::KnownHeaderName::Accept, "Known");
+    headers.insert(
+        "non-utf8",
+        vec![
+            0xC0, 0xC1, 0xF5, 0xF6, 0xF7, 0xF8, 0xF9, 0xFA, 0xFB, 0xFC, 0xFD, 0xFE, 0xFF,
+        ],
+    );
+    headers.insert("multi-values", "value1");
+    headers.append("multi-values", "value2");
+    headers.append("multi-values", "value3");
+    assert_eq!(
+        serde_json::json!({
+            "Accept": "Known",
+            "non-utf8": vec![
+                0xC0, 0xC1, 0xF5, 0xF6, 0xF7, 0xF8, 0xF9, 0xFA, 0xFB, 0xFC, 0xFD, 0xFE, 0xFF,
+            ],
+            "multi-values": ["value1", "value2", "value3"]
+        }),
+        serde_json::to_value(&headers).unwrap()
+    );
+}
