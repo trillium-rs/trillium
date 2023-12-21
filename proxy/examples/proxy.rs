f6daadbb65e06819d63845a0277263bcd382af59
Jacob Rothstein <hi@jbr.me>
2023-12-20 16:40:13 -0800

    proxy: via pseudonym can be a &'static str

--- proxy/examples/proxy.rs
+++ proxy/examples/proxy.rs
@@ -7,6 +7,6 @@ pub fn main() {
     let client_config = ClientConfig::default();
     trillium_smol::run((
         Logger::new(),
-        Proxy::new(client_config, "http://localhost:8081/"),
+        Proxy::new(client_config, "http://localhost:8081/").with_via_pseudonym("trillium-proxy"),
     ));
 }
