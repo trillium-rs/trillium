524a90a2abfdbeb984b3594615d6b53a6c850fd9
Jacob Rothstein <hi@jbr.me>
2023-12-29 13:29:18 -0800

    client runtime adapters: perform async dns lookups

--- smol/src/client.rs
+++ smol/src/client.rs
@@ -5,6 +5,7 @@ use std::{
     io::{Error, ErrorKind, Result},
 };
 use trillium_server_common::{async_trait, Connector, Transport, Url};
+use url::Host;
 
 /**
 configuration for the tcp Connector
@@ -45,25 +46,37 @@ impl Connector for ClientConfig {
     type Transport = SmolTransport<TcpStream>;
 
     async fn connect(&self, url: &Url) -> Result<Self::Transport> {
-        let socket_addrs = url.socket_addrs(|| None)?;
         if url.scheme() != "http" {
-            Err(Error::new(
+            return Err(Error::new(
                 ErrorKind::InvalidInput,
                 format!("unknown scheme {}", url.scheme()),
-            ))
-        } else {
-            let mut tcp = Self::Transport::connect(&socket_addrs[..]).await?;
-
-            if let Some(nodelay) = self.nodelay {
-                tcp.set_nodelay(nodelay)?;
-            }
-
-            if let Some(ttl) = self.ttl {
-                tcp.set_ip_ttl(ttl)?;
-            }
-
-            Ok(tcp)
+            ));
         }
+
+        let port = url
+            .port_or_known_default()
+            // this should be ok because we already checked that the scheme is http, which has a default port
+            .ok_or_else(|| Error::new(ErrorKind::InvalidInput, format!("{url} missing port")))?;
+
+        let host = url
+            .host()
+            .ok_or_else(|| Error::new(ErrorKind::InvalidInput, format!("{url} missing host")))?;
+
+        let mut tcp = match host {
+            Host::Domain(domain) => Self::Transport::connect((domain, port)).await?,
+            Host::Ipv4(ip) => Self::Transport::connect((ip, port)).await?,
+            Host::Ipv6(ip) => Self::Transport::connect((ip, port)).await?,
+        };
+
+        if let Some(nodelay) = self.nodelay {
+            tcp.set_nodelay(nodelay)?;
+        }
+
+        if let Some(ttl) = self.ttl {
+            tcp.set_ip_ttl(ttl)?;
+        }
+
+        Ok(tcp)
     }
 
     fn spawn<Fut: Future<Output = ()> + Send + 'static>(&self, fut: Fut) {
