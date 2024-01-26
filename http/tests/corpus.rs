8d1cd511119df58fbbd06852498a830e16e97ba1
Jacob Rothstein <hi@jbr.me>
2024-01-25 16:24:46 -0800

    feat(test): add corpus tests

---
+++ http/tests/corpus.rs
@@ -0,0 +1,101 @@
+use indoc::formatdoc;
+use pretty_assertions::assert_eq;
+use test_harness::test;
+use trillium_http::{Conn, KnownHeaderName, Stopper};
+use trillium_testing::{harness, TestTransport};
+const TEST_DATE: &str = "Tue, 21 Nov 2023 21:27:21 GMT";
+
+async fn handler(mut conn: Conn<TestTransport>) -> Conn<TestTransport> {
+    let response_body = formatdoc! {"
+        ===request===
+        method: {method}
+        path: {path}
+        version: {version}
+
+        ===headers===
+        {headers}
+        ",
+        method = conn.method(),
+        path = conn.path(),
+        version = conn.http_version(),
+        headers = conn.request_headers()
+    };
+
+    conn.response_headers_mut()
+        .insert(KnownHeaderName::Date, TEST_DATE);
+    conn.response_headers_mut()
+        .insert(KnownHeaderName::Server, "corpus-test");
+
+    match conn.request_body().await.read_string().await {
+        Ok(request_body) => {
+            conn.set_status(200);
+            conn.set_response_body(format!("{response_body}===body===\n{request_body}"));
+        }
+
+        Err(e) => {
+            conn.set_status(500);
+            conn.set_response_body(format!("{response_body}===error===\n{e}"));
+        }
+    };
+
+    conn
+}
+
+#[test(harness)]
+async fn corpus_test() {
+    env_logger::init();
+    let dir = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests/corpus");
+    let filter = std::env::var("CORPUS_TEST_FILTER").unwrap_or_default();
+    let corpus_request_files = std::fs::read_dir(dir)
+        .unwrap()
+        .filter_map(|f| {
+            let path = f.unwrap().path();
+            if path.extension().and_then(|x| x.to_str()) == Some("request") {
+                Some(path)
+            } else {
+                None
+            }
+        })
+        .filter(|f| f.to_str().unwrap().contains(&filter));
+
+    for file in corpus_request_files {
+        let request = std::fs::read_to_string(&file)
+            .unwrap_or_else(|_| panic!("could not read {}", file.display()))
+            .replace(['\r', '\n'], "")
+            .replace("\\r", "\r")
+            .replace("\\n", "\n");
+
+        let (client, server) = TestTransport::new();
+        let stopper = Stopper::new();
+        let res = trillium_testing::spawn({
+            let stopper = stopper.clone();
+            async move { Conn::map(server, stopper, handler).await }
+        });
+
+        client.write_all(request);
+        let (response, extension) = match res.await.unwrap() {
+            Ok(None) => (client.read_available_string().await, "response"),
+            Err(e) => (e.to_string(), "error"),
+            Ok(Some(_)) => ("".to_string(), "upgrade"),
+        };
+
+        let response_file = file.with_extension(extension);
+
+        if option_env!("CORPUS_TEST_WRITE").is_some() {
+            std::fs::write(
+                response_file,
+                response.replace('\r', "\\r").replace('\n', "\\n\n"),
+            )
+            .unwrap();
+        } else {
+            let expected_response = std::fs::read_to_string(response_file)
+                .unwrap()
+                .replace(['\n', '\r'], "")
+                .replace("\\r", "\r")
+                .replace("\\n", "\n");
+            assert_eq!(expected_response, response, "{file:?}");
+        }
+
+        stopper.stop();
+    }
+}
