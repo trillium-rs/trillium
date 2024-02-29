23ed4f262d55426db5e2a9c97c91ce04c06d4cca
Jacob Rothstein <hi@jbr.me>
2024-02-29 15:35:33 -0800

    feat!: upgrade rustls, use platform verifier

--- rustls/src/server.rs
+++ rustls/src/server.rs
@@ -1,12 +1,16 @@
-use crate::RustlsTransport;
-use futures_rustls::TlsAcceptor;
-use rustls::ServerConfig;
+use futures_rustls::{
+    rustls::{ServerConfig, ServerConnection},
+    server::TlsStream,
+    TlsAcceptor,
+};
 use std::{
     fmt::{Debug, Formatter},
     io,
+    pin::Pin,
     sync::Arc,
+    task::{Context, Poll},
 };
-use trillium_server_common::{async_trait, Acceptor, Transport};
+use trillium_server_common::{async_trait, Acceptor, AsyncRead, AsyncWrite, Transport};
 
 /**
 trillium [`Acceptor`] for Rustls
@@ -16,7 +20,7 @@ trillium [`Acceptor`] for Rustls
 pub struct RustlsAcceptor(TlsAcceptor);
 impl Debug for RustlsAcceptor {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        f.debug_tuple("RustTls").field(&"<<TlsAcceptor>>").finish()
+        f.debug_tuple("Rustls").field(&"<<TlsAcceptor>>").finish()
     }
 }
 
@@ -29,7 +33,9 @@ impl RustlsAcceptor {
     }
 
     /**
-    build a new RustlsAcceptor from a cert chain and key. See
+    build a new RustlsAcceptor from a cert chain (pem) and private key.
+
+    See
     [`ConfigBuilder::with_single_cert`][`crate::rustls::ConfigBuilder::with_single_cert`]
     for accepted formats. If you need to customize the
     [`ServerConfig`], use ServerConfig's Into RustlsAcceptor, eg
@@ -52,31 +58,20 @@ impl RustlsAcceptor {
     let rustls_acceptor = RustlsAcceptor::from_single_cert(CERT, KEY);
     ```
     */
-    #[cfg(any(feature = "ring", feature = "aws-lc-rs"))]
     pub fn from_single_cert(cert: &[u8], key: &[u8]) -> Self {
         use std::io::Cursor;
 
-        let mut br = Cursor::new(cert);
-        let certs = rustls_pemfile::certs(&mut br)
-            .collect::<Result<Vec<_>, _>>()
+        let cert_chain = rustls_pemfile::certs(&mut Cursor::new(cert))
+            .collect::<Result<_, _>>()
             .expect("could not read certificate");
 
-        let mut br = Cursor::new(key);
-        let key = rustls_pemfile::pkcs8_private_keys(&mut br)
-            .next()
-            .expect("no pkcs8 private key found in `key`")
-            .expect("could not read key pemfile");
+        let key_der = rustls_pemfile::private_key(&mut Cursor::new(key))
+            .expect("could not read key pemfile")
+            .expect("no private key found in `key`");
 
-        #[cfg(all(feature = "ring", not(feature = "aws-lc-rs")))]
-        let provider = rustls::crypto::ring::default_provider();
-        #[cfg(feature = "aws-lc-rs")]
-        let provider = rustls::crypto::aws_lc_rs::default_provider();
-
-        ServerConfig::builder_with_provider(Arc::new(provider))
-            .with_safe_default_protocol_versions()
-            .expect("could not enable default TLS versions")
+        ServerConfig::builder()
             .with_no_client_auth()
-            .with_single_cert(certs, key.into())
+            .with_single_cert(cert_chain, key_der)
             .expect("could not create a rustls ServerConfig from the supplied cert and key")
             .into()
     }
@@ -94,14 +89,96 @@ impl From<TlsAcceptor> for RustlsAcceptor {
     }
 }
 
+/// Transport for rustls server acceptor
+#[derive(Debug)]
+pub struct RustlsServerTransport<T>(TlsStream<T>);
+
+impl<T: AsyncRead + AsyncWrite + Unpin> AsyncRead for RustlsServerTransport<T> {
+    fn poll_read(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut [u8],
+    ) -> Poll<io::Result<usize>> {
+        Pin::new(&mut self.0).poll_read(cx, buf)
+    }
+}
+
+impl<T: AsyncWrite + AsyncRead + Unpin> AsyncWrite for RustlsServerTransport<T> {
+    fn poll_write(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<io::Result<usize>> {
+        Pin::new(&mut self.0).poll_write(cx, buf)
+    }
+
+    fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        Pin::new(&mut self.0).poll_flush(cx)
+    }
+
+    fn poll_close(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        Pin::new(&mut self.0).poll_close(cx)
+    }
+
+    fn poll_write_vectored(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        bufs: &[io::IoSlice<'_>],
+    ) -> Poll<io::Result<usize>> {
+        Pin::new(&mut self.0).poll_write_vectored(cx, bufs)
+    }
+}
+
+impl<T: Transport> Transport for RustlsServerTransport<T> {
+    fn peer_addr(&self) -> io::Result<Option<std::net::SocketAddr>> {
+        self.inner_transport().peer_addr()
+    }
+}
+
+impl<T> RustlsServerTransport<T> {
+    /// access the contained transport type (eg TcpStream)
+    pub fn inner_transport(&self) -> &T {
+        self.0.get_ref().0
+    }
+
+    /// mutably access the contained transport type (eg TcpStream)
+    pub fn inner_transport_mut(&mut self) -> &mut T {
+        self.0.get_mut().0
+    }
+}
+
+impl<T> AsRef<ServerConnection> for RustlsServerTransport<T> {
+    fn as_ref(&self) -> &ServerConnection {
+        self.0.get_ref().1
+    }
+}
+
+impl<T> AsMut<ServerConnection> for RustlsServerTransport<T> {
+    fn as_mut(&mut self) -> &mut ServerConnection {
+        self.0.get_mut().1
+    }
+}
+
+impl<T> From<TlsStream<T>> for RustlsServerTransport<T> {
+    fn from(value: TlsStream<T>) -> Self {
+        Self(value)
+    }
+}
+
+impl<T> From<RustlsServerTransport<T>> for TlsStream<T> {
+    fn from(RustlsServerTransport(value): RustlsServerTransport<T>) -> Self {
+        value
+    }
+}
+
 #[async_trait]
 impl<Input> Acceptor<Input> for RustlsAcceptor
 where
     Input: Transport,
 {
-    type Output = RustlsTransport<Input>;
+    type Output = RustlsServerTransport<Input>;
     type Error = io::Error;
     async fn accept(&self, input: Input) -> Result<Self::Output, Self::Error> {
-        self.0.accept(input).await.map(RustlsTransport::from)
+        self.0.accept(input).await.map(RustlsServerTransport)
     }
 }
