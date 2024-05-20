25ce05e22c0c9410d4ced31935cc05dc4134ed53
Jacob Rothstein <hi@jbr.me>
2024-05-20 14:03:37 -0700

    fix: fastrand seed change

--- conn-id/tests/tests.rs
+++ conn-id/tests/tests.rs
@@ -1,12 +1,19 @@
+use std::sync::atomic::{AtomicUsize, Ordering};
+
 use trillium_conn_id::*;
 use trillium_testing::{prelude::*, TestConn};
 use uuid::Uuid;
 
+fn build_incrementing_id_generator() -> impl Fn() -> String + Send + Sync + 'static {
+    let count = AtomicUsize::default();
+    move || count.fetch_add(1, Ordering::SeqCst).to_string()
+}
+
 #[test]
 fn test_defaults() {
-    let app = (ConnId::new().with_seed(1000), "ok");
-    assert_ok!(get("/").on(&app), "ok", "x-request-id" => "4fekClhof7");
-    assert_ok!(get("/").on(&app), "ok", "x-request-id" => "PAmkU1LPSe");
+    let app = (ConnId::new().with_seed(1), "ok");
+    assert_ok!(get("/").on(&app), "ok", "x-request-id" => "ohOwAwv0lK");
+    assert_ok!(get("/").on(&app), "ok", "x-request-id" => "DJA1GZigip");
     assert_ok!(
         get("/").with_request_header("x-request-id", "inbound-id").on(&app),
         "ok",
@@ -14,8 +21,8 @@ fn test_defaults() {
     );
 
     let conn = get("/").on(&app);
-    assert_eq!(conn.id(), "kZTgZfbUJB");
-    assert_eq!(log_formatter::conn_id(&conn, true), "kZTgZfbUJB");
+    assert_eq!(conn.id(), "mv15faFHFq");
+    assert_eq!(log_formatter::conn_id(&conn, true), "mv15faFHFq");
 
     let conn = TestConn::build("get", "/", ());
     assert_eq!(log_formatter::conn_id(&conn, true), "-");
@@ -48,7 +55,7 @@ fn test_settings() {
 fn test_no_headers() {
     let app = (
         ConnId::new()
-            .with_seed(1000)
+            .with_id_generator(build_incrementing_id_generator())
             .without_request_header()
             .without_response_header(),
         "ok",
@@ -56,11 +63,11 @@ fn test_no_headers() {
 
     let conn = get("/").on(&app);
     assert!(conn.response_headers().get("x-request-id").is_none());
-    assert_eq!(conn.id(), "4fekClhof7");
+    assert_eq!(conn.id(), "0");
 
     let conn = get("/")
         .with_request_header("x-request-id", "ignored")
         .on(&app);
-    assert_eq!(conn.id(), "PAmkU1LPSe");
+    assert_eq!(conn.id(), "1");
     assert!(conn.response_headers().get("x-request-id").is_none());
 }
